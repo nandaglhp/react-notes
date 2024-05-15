d2e61c75be332ae6d715e228e3afd28ca1406d6c
= <=>
2024-05-15 15:00:11 +0700

    useAuth-comments

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -2,9 +2,14 @@ import * as React from "react";
 
 import { AuthContext } from "@/context/authContext";
 
+// ketika dipanggil akan mengakses konteks autentikasi.
 export const useAuth = () => {
-    const context = React.useContext(AuthContext);
-    if (!context)
-        throw new Error("useAuth must be used within an AuthProvider");
-    return context;
+  // useContext adalah hook yang disediakan oleh React untuk mengonsumsi konteks yang telah didefinisikan
+  // mengakses nilai yang saat ini disediakan oleh AuthContext.
+  const context = React.useContext(AuthContext);
+  // memastikan bahwa context tidak null. Jika context null, itu berarti useAuth
+  // digunakan di luar komponen yang dibungkus oleh AuthProvider.
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  // mengembalikan context yang telah diperoleh dari AuthContext
+  return context;
 };
