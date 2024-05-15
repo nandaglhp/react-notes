6602cca2beae46dd1cae306bafe4a13866be4184
= <=>
2024-05-15 12:55:43 +0700

    comments-router

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -12,34 +12,40 @@ import Register from "@/pages/auth/Register";
 import SingleNote from "@/pages/notes/SingleNote";
 
 export default function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Protected />}>
-                    <Route element={<AppLayouts />}>
-                        <Route index element={<Home />} />
-                    </Route>
+  return (
+    // Pembungkus utama history API (navigasi tanpa reload halaman.)
+    <BrowserRouter>
+      {/* Pembungkus untuk koleksi Route */}
+      <Routes>
+        {/* Komponen yang mendefinisikan rute individu dalam aplikasi. */}
+        {/* Protected:  memeriksa apakah pengguna telah terotentikasi sebelum mengizinkan akses ke rute anak. */}
+        <Route path="/" element={<Protected />}>
+          {/* element = menentukan komponen yang harus dirender ketika rute tertentu dicocokkan. */}
+          <Route element={<AppLayouts />}>
+            <Route index element={<Home />} />
+          </Route>
 
-                    <Route path="/notes" element={<NoteLayout />}>
-                        <Route index element={<EmptyNote />} />
-                        <Route path=":id" element={<SingleNote />} />
-                    </Route>
+          <Route path="/notes" element={<NoteLayout />}>
+            <Route index element={<EmptyNote />} />
+            <Route path=":id" element={<SingleNote />} />
+          </Route>
 
-                    <Route path="/archived" element={<NoteLayout archived />}>
-                        <Route index element={<EmptyNote />} />
-                        <Route path=":id" element={<SingleNote />} />
-                    </Route>
+          {/* archived true */}
+          <Route path="/archived" element={<NoteLayout archived />}>
+            <Route index element={<EmptyNote />} />
+            <Route path=":id" element={<SingleNote />} />
+          </Route>
 
-                    <Route path="/create" element={<NoteLayout />}>
-                        <Route index element={<FormNote />} />
-                    </Route>
-                </Route>
+          <Route path="/create" element={<NoteLayout />}>
+            <Route index element={<FormNote />} />
+          </Route>
+        </Route>
 
-                <Route path="/auth" element={<AuthLayout />}>
-                    <Route path="login" element={<Login />} />
-                    <Route path="register" element={<Register />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
